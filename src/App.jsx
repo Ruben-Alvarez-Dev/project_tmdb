5909939b67abc95d155c3f9e2cf775668e9e9449
ruben <ruben@IMACPRO.local>
2024-02-09 10:07:13 +0100

    fetchMovies function implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
   const API_KEY = "fc1f80b194f3f02aff9e1973e07870eb";
   const IMAGE_PATH = "http://image.tmdb.org/t/p/original";
   const URL_IMAGE = "http://image.tmdb.org/t/p/original";
+  /* const params = {
+    params: {
+      api_key: API_KEY,
+      query: searchKey
+  } */
 
   // State vars
   const [movies, setMovies] = useState([]);
@@ -14,6 +20,19 @@ function App() {
   const [trailer, setTrailer] = useState(null);
   const [movie, setMovie] = useState({ title: "Loading movies..." });
   const [playing, setPlaying] = useState(false);
+
+  // Movie List fetch function
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/movie/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+  };
 
   return (
     <>
