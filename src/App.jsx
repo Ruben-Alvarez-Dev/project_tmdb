9db94d15c409c227bf121cd3a09afc467f8e899c
ruben <ruben@IMACPRO.local>
2024-02-09 13:18:01 +0100

    video playing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import YouTube from "react-youtube";
 
 function App() {
   // Global information
   const API_URL = "https://api.themoviedb.org/3";
   const API_KEY = "fc1f80b194f3f02aff9e1973e07870eb";
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 
   // State vars
   const [movies, setMovies] = useState([]);
@@ -43,7 +45,7 @@ function App() {
         append_to_response: "videos",
       },
     });
-    if (data.videos & data.videos.results) {
+    if (data.videos && data.videos.results) {
       const trailer = data.videos.results.find(
         (vid) => vid.name === "Official Trailer"
       );
@@ -65,8 +67,19 @@ function App() {
     fetchMovies(searchKey);
   };
 
+  /* useEffect(() => {
+    fetchMovies();
+    if (movies.length > 0) {
+      fetchMovie(movies[0].id);
+    }
+  }, []); */
+
   useEffect(() => {
     fetchMovies();
+    if (movies.length > 0) {
+      const firstMovie = movies[0];
+      fetchMovie(firstMovie);
+    }
   }, []);
 
   return (
@@ -81,11 +94,74 @@ function App() {
         />
         <button onClick={searchMovies}>Search</button>
       </form>
+
+      {/* Banner and Video Container */}
+      <div>
+        <main>
+          {movie ? (
+            <div
+              className="viewtrailer"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    className="player"
+                    opts={{
+                      height: "100%",
+                      width: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      },
+                    }}
+                  />
+                  <button onClick={() => setPlaying(false)} className="btn">
+                    Close
+                  </button>
+                </>
+              ) : (
+                <div className="container">
+                  <div className="">
+                    {trailer ? (
+                      <button
+                        className="btn"
+                        onClick={() => setPlaying(true)}
+                        type="btn"
+                      >
+                        Play Trailer
+                      </button>
+                    ) : (
+                      "Sotty, no trailer available."
+                    )}
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
+
       {/* Container */}
       <div className="container">
         <div className="display">
           {movies.map((movie) => (
-            <div key={movie.id} className="movieCard">
+            <div
+              key={movie.id}
+              className="movieCard"
+              onClick={() => selectMovie(movie)}
+            >
               <img src={`${URL_IMAGE + movie.poster_path}`} alt="" />
               <h4>{movie.title}</h4>
             </div>
