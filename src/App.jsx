98047f5888e78c59c0690a41ee21bb6071bf7483
ruben <ruben@IMACPRO.local>
2024-02-09 09:43:44 +0100

    global consts declared

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Global information
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "fc1f80b194f3f02aff9e1973e07870eb";
+  const IMAGE_PATH = "http://image.tmdb.org/t/p/original";
+  const URL_IMAGE = "http://image.tmdb.org/t/p/original";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, world!</h1>
+      <p>Welcome to the React 18 Alpha!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tmdb.md
@@ -0,0 +1,7 @@
+<!-- Clave de la API -->
+
+'fc1f80b194f3f02aff9e1973e07870eb'
+
+<!-- Token de acceso de lectura a la API -->
+
+'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzFmODBiMTk0ZjNmMDJhZmY5ZTE5NzNlMDc4NzBlYiIsInN1YiI6IjY0ZGE3ZDA5YmYzMWYyMDFjY2MwMjI5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9sx4LvoFY_tf3GBe96QtR_0wWw6C80n-SXdDXd123TU'
