8891a0c89c1ff9f375c60f671f7c4233c5cc388f
ruben <ruben@IMACPRO.local>
2024-02-09 09:47:48 +0100

    state vars declared

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ function App() {
   const IMAGE_PATH = "http://image.tmdb.org/t/p/original";
   const URL_IMAGE = "http://image.tmdb.org/t/p/original";
 
+  // State vars
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading movies..." });
+  const [playing, setPlaying] = useState(false);
+
   return (
     <>
       <h1>Hello, world!</h1>
